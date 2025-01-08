d7b500bb9ce67d0b825698893c9ac760ebdd9bf8
arineto1 <ari.neto.t@gmail.com>
2025-01-08 17:26:05 -0300

    commit 5

---
+++ informaticaInternet/primeiro/interfacesWebI/interfacesWeb.js
@@ -0,0 +1,8 @@
+function aula1() {
+    var iframe = document.getElementById('aulaPDF')
+    iframe.src = 'aulas/aula1.pdf#zoom=80'
+  }
+  function aula2() {
+    var iframe = document.getElementById('aulaPDF')
+    iframe.src = 'aulas/aula2.pdf#zoom=80'
+  }
